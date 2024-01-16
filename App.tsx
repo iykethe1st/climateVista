aeaf25c5303bc33d2844eb81d68461cbf52cbdef
Ikenna <iykethe1st@gmail.com>
2024-01-16 18:34:47 +0100

    added home component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,59 @@
-import { Text, SafeAreaView, View } from "react-native";
-import React from "react";
+import {Text, SafeAreaView, View, StyleSheet} from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Current Weather</Text>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect sweatshirt weather</Text>
       </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'teal',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    color: 'black',
+    fontSize: 48,
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30,
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20,
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  description: {
+    fontSize: 48,
+  },
+  message: {
+    fontSize: 30,
+  },
+});
 
 export default App;
