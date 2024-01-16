fe11f2826fbbe120106797dd08d1d02cd0871a00
Ikenna <iykethe1st@gmail.com>
2024-01-16 18:08:19 +0100

    initialized react-native

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { Text, SafeAreaView, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Current Weather</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
