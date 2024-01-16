a860c08348d9f3768a67d36ca242ba1316c0a860
Ikenna <iykethe1st@gmail.com>
2024-01-16 18:58:30 +0100

    fixed imports

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import CurrentWeather from './src/components/currentWeather/CurrentWeather';
+import CurrentWeather from './src/components/CurrentWeather';
+import UpcomingWeather from './src/components/UpcomingWeather';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <CurrentWeather />
+      <UpcomingWeather />
     </View>
   );
 }
---
+++ src/components/UpcomingWeather.tsx
@@ -0,0 +1,16 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const UpcomingWeather = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>UpcomingWeather</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default UpcomingWeather;
