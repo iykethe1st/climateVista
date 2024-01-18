be7ac04507544ebe89aab322febce9532675403e
Ikenna <iykethe1st@gmail.com>
2024-01-18 09:10:00 +0100

    added react native dotenv

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
 import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import * as Location from 'expo-location';
+
+// https://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={API key}
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
@@ -15,6 +18,20 @@ export default function App() {
       </View>
     );
   }
+
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('permission to access location was denied');
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if (location) console.log({location});
+
   return (
     <NavigationContainer>
       <Tabs />
